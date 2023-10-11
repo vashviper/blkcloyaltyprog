ab2c1f33f8aef40c1498bcf21ac155677bb7fa8b
vashviper <sheraj.hussein@gmail.com>
2023-10-12 00:42:32 +0800

    working but no merchants - comments

--- contracts/loyaltyprogram2.sol
+++ contracts/loyaltyprogram2.sol
@@ -2,16 +2,16 @@
 pragma solidity ^0.8.18;
 
 contract LoyaltyProgram {
-    address public owner;
+    address public owner; //contract owner
     
     struct Customer {
-        uint256 balance;
-        bool isRegistered;
+        uint256 balance; // can be called to check the balance points
+        bool isRegistered; //customer need to register or they will not be able to earn or redeem points
     }
     
     mapping(address => Customer) public customers;
-    mapping(uint256 => uint256) public rewards;
-    mapping(uint256 => string) public rewardNames;
+    mapping(uint256 => uint256) public rewards; // the rewards are redeemed via rewardID
+    mapping(uint256 => string) public rewardNames; // rewardID can be assigned a reward description
     
     event Registered(address customer);
     event PointsEarned(address customer, uint256 pointsEarned);
@@ -29,33 +29,33 @@ contract LoyaltyProgram {
     
     function register() public {
         require(!customers[msg.sender].isRegistered, "Customer is already registered");
-        customers[msg.sender] = Customer(100, true);
+        customers[msg.sender] = Customer(100, true); // Upon registering the customer earns 100 points
         emit Registered(msg.sender);
     }
     
     function earnPoints(uint256 points) public {
-        require(customers[msg.sender].isRegistered, "Customer is not registered");
-        customers[msg.sender].balance += points;
+        require(customers[msg.sender].isRegistered, "Customer is not registered"); //if customer did not register then points cannot be earned
+        customers[msg.sender].balance += points; // for adding points to balance points
         emit PointsEarned(msg.sender, points);
     }
     
     function checkBalance() public view returns (uint256) {
-        return customers[msg.sender].balance;
+        return customers[msg.sender].balance;// for checking the balance points
     }
     
     function redeemPoints(uint256 rewardId) public {
         require(customers[msg.sender].isRegistered, "Customer is not registered");
         require(rewards[rewardId] > 0, "Reward not found");
-        require(customers[msg.sender].balance >= rewards[rewardId], "Insufficient points");
+        require(customers[msg.sender].balance >= rewards[rewardId], "Insufficient points"); // if customer balance points is less than the reward points
         
-        customers[msg.sender].balance -= rewards[rewardId];
+        customers[msg.sender].balance -= rewards[rewardId]; // for deducting reward points from balance points
         emit PointsRedeemed(msg.sender, rewards[rewardId], rewardId);
-        // Implement reward distribution logic here
+        // Implement reward distribution logic here : To transact customer need
     }
     
     function addReward(uint256 rewardId, uint256 pointsRequired, string memory rewardName) public onlyOwner {
-        rewards[rewardId] = pointsRequired;
-        rewardNames[rewardId] = rewardName;
+        rewards[rewardId] = pointsRequired; // rewardID is used to redeem, need to assign a number and points
+        rewardNames[rewardId] = rewardName; // assign a reward name to a reward id
         emit RewardAdded(rewardId, pointsRequired, rewardName);
     }
 }
